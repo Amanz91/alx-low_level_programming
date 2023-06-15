f81886d92971c35a2288acc0e8f2725518ce00e6
Amanz91 <findingmanzilla@gmail.com>
2023-06-15 15:53:21 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program to print a line with printf function
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
