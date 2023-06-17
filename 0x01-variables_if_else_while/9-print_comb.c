bc23d830ee985e6251680b27007306fe46621776
Amanz91 <findingmanzilla@gmail.com>
2023-06-17 13:57:13 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible combinations of single-digit numbers
+ * Retirn: 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '8' ; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+	}
+	for (i = '9')
+	{
+		putchar(i);
+	}
+return (0);
+}
