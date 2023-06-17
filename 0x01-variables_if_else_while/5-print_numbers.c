a681cc2658d81aff62a9a5fbdaaf6ba67eb915e5
Amanz91 <findingmanzilla@gmail.com>
2023-06-17 12:34:27 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - print a to z followed by A to Z
- *
+ * main - A C program to print zero to nine with printf function
  * Return: 0 (Success)
-*/
+ */
 int main(void)
-{ printf("0123456789\n")
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
