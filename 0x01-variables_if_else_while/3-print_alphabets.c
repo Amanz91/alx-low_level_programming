cb987f5d935669f2fe11b962b64961fcc1668d01
Amanz91 <findingmanzilla@gmail.com>
2023-06-17 09:14:09 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main - print a to z followed by A to Z
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	char c;
+	char C;
+
+	for ((c = 'a' ; c <= 'z' ; c++) (C = 'A' ; C <= 'Z' ; C++))
+	{
+		putchar(cC);
+	}
+putchar('\n')
+return (0);
+}
