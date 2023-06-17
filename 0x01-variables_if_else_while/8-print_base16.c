76d1f7341884501bd623a9e8f812699580e83ad6
Amanz91 <findingmanzilla@gmail.com>
2023-06-17 13:10:44 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - program to print  all the numbers of base sixteen  in lowercase
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+	}
+	for (c = 'a' ; c <= 'f' ; c++)
+	{
+		putchar(c);
+	}
+putchar('\n')
+return (0);
+}
