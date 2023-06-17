5018c9288e4bb26d668fad76cef91a90faadb922
Amanz91 <findingmanzilla@gmail.com>
2023-06-17 12:07:45 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program to print numbers between zero and nine
+ *
+ * Return: 0 (Success)
+*/
+int manin (void)
+{
+	int i;
+
+	for (i = '0' ; i <= '0' ; i++)
+	{
+		putchar(i);
+	}
+putchar('\n')
+return (0);
+}
