f230190cf8f5f069a17dbd04be62d6fa6e95da84
Amanz91 <findingmanzilla@gmail.com>
2023-06-17 09:59:18 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,14 @@ int main(void)
 	char c;
 	char C;
 
-	for ((c = 'a' ; c <= 'z' ; c++) (C = 'A' ; C <= 'Z' ; C++))
+	for (c = 'a' ; c <= 'z' ; c++)
 	{
 		putchar(c);
+	}
+	for (C = 'A' ; C <= 'Z' ; C++)
+	{
 		putchar(C);
 	}
-putchar('\n')
+putchar('\n');
 return (0);
 }
