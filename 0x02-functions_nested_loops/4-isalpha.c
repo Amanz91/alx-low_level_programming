90d4de1cc03f92510b95046fe9ffa018be0a9af9
Amanz91 <findingmanzilla@gmail.com>
2023-06-20 13:14:49 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - program to check for lowercase character
+ * @c: character to be checked
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		if (c >= 'A' && c <= 'Z')
+		{
+		return (1);
+		}
+	}
+	return (0);
+}
