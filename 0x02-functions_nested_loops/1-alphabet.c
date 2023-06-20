19dd6c58e8491cce401b8d67608efd197d3bd5bb
Amanz91 <findingmanzilla@gmail.com>
2023-06-20 12:06:11 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - program to print lower case alphabet
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_puthchar('\n')
+}
