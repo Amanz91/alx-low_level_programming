a69afb7200b0cfa4471438d19618ffe9fb964ad5
Amanz91 <findingmanzilla@gmail.com>
2023-06-20 12:52:47 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - program to check for lowercase character
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	int c;
+	int i = 1;
+	int j = 0;
+
+	if (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(i);
+	}
+	else
+	_putchar(j);
+	}
+}
