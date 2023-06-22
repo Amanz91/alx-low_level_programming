a21d5ddfdf56f7c0afdd0747a98ccf9aabfa30d8
Amanz91 <findingmanzilla@gmail.com>
2023-06-22 14:42:53 -0700

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int mul - a function that multiplies two integers
+ * mul - a function that multiplies two integers
  * @a: first number to be multiplied
  * @b: second number to be multiplied
  * Return: integer value of mul
