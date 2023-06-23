b7f6b82b5ad172291bf0c9193f70825caec762e4
Amanz91 <findingmanzilla@gmail.com>
2023-06-23 15:30:28 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangles - a function that draws a triangle with # in the terminal
+ * @size: size of square
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int r, h, s;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (r = 1; r < size; r++)
+		{
+			for (s = size - r; s >= 1; s--)
+			{
+				_putchar(' ');
+			}
+			for (h = 1; h <= r; h++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
