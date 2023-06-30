1d5352474ae348057ce96c53ad580020b6981723
Amanz91 <findingmanzilla@gmail.com>
2023-06-30 08:10:25 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - a function that reverses a string.
+ * @a: string to be reversed
+ * @n: source string length
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	char b;
+
+	for (i = 0; i <= (n / 2); i++)
+	{
+		b = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = b;
+	}
+	return (a);
+}
