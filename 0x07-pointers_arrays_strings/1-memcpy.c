365ef45c5c87248d7ba8018fb0316d5d7f8ebee7
Amanz91 <findingmanzilla@gmail.com>
2023-07-04 08:40:08 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,7 @@
  * _memcpy - a function that copies memory area
  * @dest: where string is copied to
  * @src: where srting is copied from
- * @n: bytes from memory area 
+ * @n: bytes from memory area
  * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
