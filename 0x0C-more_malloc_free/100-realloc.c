96549f42e7c952fb83aada679b3e251d2816c925
Amanz91 <findingmanzilla@gmail.com>
2023-07-14 09:04:56 -0700

    task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - a function that reallocates a memory block using malloc and free
+ * @ptr: pointer to array
+ * @old_size: size of ptr
+ * @new_size: new size
+ * Return: pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *ptr_new;
+	char *ptr_old;
+	unsigned int i;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (old_size == new_size)
+		return (ptr);
+	if (ptr != NULL && new_size == 0)
+	{
+		return (NULL);
+		free(ptr);
+	}
+	ptr_old = ptr;
+	ptr_new = malloc(new_size);
+	if (ptr_new == NULL)
+		return (NULL);
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+		{
+			ptr_new[i] = ptr_old[i];
+		}
+	}
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+		{
+			ptr_new[i] = ptr_old[i];
+		}
+	}
+	return (ptr_new);
+	free(ptr);
+}
