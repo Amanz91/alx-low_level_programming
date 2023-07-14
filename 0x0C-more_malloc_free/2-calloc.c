4d4843ca341090a3d3d069c8e684a15e763e70a1
Amanz91 <findingmanzilla@gmail.com>
2023-07-14 07:29:23 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _memset - a function that fills memory with a constant byte.
+ * @s: pointer to string
+ * @b: constant byte
+ * @n: number of bytes to be filled
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
+/**
+ * _calloc - a function that allocates memory for an array, using malloc
+ * @nmemb: number of elements of array
+ * @size: size of memory
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * sizee);
+	if (p == NULL)
+		return (NULL);
+	_memset(p, 0, nmemb * size);
+	return (p);
+}
