5321f57c734b72d2973524d2be4230a2aaee1d36
Amanz91 <fimdingmanzilla@gmail.com>
2023-07-17 11:07:28 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct called dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+ *
+ * Description: a struct to tell name and age of dog and name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
