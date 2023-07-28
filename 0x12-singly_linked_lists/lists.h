cfb236e72362393be7d2e237e61014b829e98f0b
Amanz91 <findingmanzilla@gmail.com>
2023-07-28 08:53:04 -0700

    task 4

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,4 +28,5 @@ size_t list_len(const list_t *h);
 size_t my_strlen(const char *s);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 #endif
