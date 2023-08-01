9d9aef7476e33cfa406b70aacb469355f6be6f5a
Amanz91 <findingmanzilla@gmail.com>
2023-08-01 10:34:42 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,14 +32,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 
 	if (idx == 0)
-        {
+	{
 		new->next = *head;
 		*head = new;
-                return (new);
-        }
+		return (new);
+	}
 	else
 	{
-		while (i < (idx-1))
+		while (i < (idx - 1))
 		{
 			temp = temp->next;
 			i++;
