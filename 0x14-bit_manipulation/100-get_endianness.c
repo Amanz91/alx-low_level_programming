7becf1e1d0f3de306708199e29863ca4eed120df
Amanz91 <findingmanzilla@gmail.com>
2023-08-07 07:55:59 -0700

    task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - a function that checks the endianness.
+ * Return: 0 or 1
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *check = ((char *) &i);
+
+	return (*check);
+}
