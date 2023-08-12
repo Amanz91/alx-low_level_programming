2b8e986d3607b48941a5730a42e3d027ae63c0b2
Amanz91 <findingmanzilla@gmail.com>
2023-08-12 01:46:13 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	while (text_content != NULL && text_content[l] != '\0')
 	{
-		l++
+		l++;
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(filename, text_content, l);
