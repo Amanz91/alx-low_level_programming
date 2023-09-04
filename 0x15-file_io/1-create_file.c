7c61493258fb26e4c9436877c41559df6593beab
Amanz91 <findingmanzill@gmail.com>
2023-09-04 06:20:59 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - a function that creates a file
+ * @filename: a pointer to file to be created
+ * @text_content: a pointer to string to be written
+ * Return: -1 or 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr, len = 0;
+
+	if (!filename)
+		return (-1);
+
+	while (text_content[len] != NULL)
+	{
+		++len;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(fd, text_content, len);
+
+	if (fd == -1 || wr == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
