da09e530e24925521a124a0c76052379b53e9e77
Amanz91 <findingmanzilla@gmail.com>
2023-10-16 07:14:19 -0700

    task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * dlistint_t *add_dnodeint - a func that adds element to a list
+ * @head: a double pointer to list
+ * @n: element to be added
+ * Return: address of new element
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp, *new;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+		*head = new = temp;
+	else
+	{
+		while (temp->prev != NULL)
+		{
+			temp = temp->prev;
+		}
+		temp->prev = new;
+		new->next = temp;
+		temp = new;
+	}
+	return (temp);
+}
