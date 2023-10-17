56f2c6305787696b6caa81c82829496632b10478
Amanz91 <findingmanzilla@gmail.com>
2023-10-17 09:10:01 -0700

    task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * int delete_dnodeint_at_index - a func that deletes nth node of a list
+ * @head: a double pointer to list
+ * @index: index
+ * Return: 1 or -1
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp;
+
+	temp = *h;
+
+	if (*head == NULL)
+		return (-1);
+	for (; idx != 1; idx--)
+	{
+		if (temp == NULL)
+			return (-1);
+		temp = temp->next;
+	}
+	if (temp == *head)
+	{
+		*head = temp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		temp->prev->next = temp->next;
+		if (tmp->next != NULL)
+			temp->next->prev = temp->prev;
+	}
+	free(temp);
+	return (1);
+}
