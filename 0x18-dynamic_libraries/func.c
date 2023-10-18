59911afe434322b3567eec5e5a2dac00a26a7388
Amanz91 <findingmanzilla@gmail.com>
2023-10-18 09:39:12 -0700

    task 2

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+/**
+ * add - a func to add nums
+ * @a: 1st arg
+ * @b: 2nd arg
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - a func to subtract nums
+ * @a: 1st arg
+ * @b: 2nd arg
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * mul - a func to multiply nums
+ * @a: 1st arg
+ * @b: 2nd arg
+ * Return: product
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * div - a func to divide nums
+ * @a: 1st arg
+ * @b: 2nd arg
+ * Return: division
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * mod - a func to divide nums and find reaminder
+ * @a: 1st arg
+ * @b: 2nd arg
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
